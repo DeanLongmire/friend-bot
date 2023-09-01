c5a3353f62716243ba1a2f4b7403a91e449b965d
Dean Longmire <deanlongmire100@gmail.com>
2023-09-01 14:19:08 -0400

    Created some basic command files

---
+++ commands/hello.js
@@ -0,0 +1,10 @@
+const { slashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('hello')
+		.setDescription('Replies with Hello!'),
+	async execute(interaction) {
+		await interaction.reply('Hello!');
+	},
+};
