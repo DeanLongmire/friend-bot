02813baea69eddbeb25d3d59bb60e007cde217a2
Dean Longmire <deanlongmire100@gmail.com>
2023-09-01 15:19:31 -0400

    Add deploy script

--- commands/fun/hello.js
+++ commands/fun/hello.js
@@ -1,10 +1,33 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const friendlyHelloMessages = [
+	"Hello Friend!",
+	"Hello *!",
+	"Hi *, how are you?",
+	"Whadup *",
+	"Nice to see you, *"
+]
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('hello')
 		.setDescription('Replies with Hello!'),
 	async execute(interaction) {
-		await interaction.reply('Hello!');
+		let msg;
+		let msgIndex;
+		if(interaction.user.username == 'cprock5') {
+
+		} else {
+			const friendlyMsgListLenght = friendlyHelloMessages.length;
+			msgIndex = getRandomInt(0,friendlyMsgListLenght);
+			msg = friendlyHelloMessages[msgIndex].replace(interaction.user.username);
+		}
+		await interaction.reply(msg);
 	},
 };
