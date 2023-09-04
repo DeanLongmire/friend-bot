9599be55dc0e813ace6046bc125a7470df31bc3e
Dean Longmire <deanlongmire100@gmail.com>
2023-09-04 16:36:42 -0400

    Mess with colin

--- commands/fun/hello.js
+++ commands/fun/hello.js
@@ -8,6 +8,20 @@ const friendlyHelloMessages = [
 	"Nice to see you, *"
 ]
 
+//msgs not for the faint of heart, we like colin I swear
+const colinHelloMessages = [
+	"Shut the fuck up",
+	"ew",
+	"Hello, Fuck Face",
+	"You literally failed out of Duke",
+	"Eat shit",
+	"...",
+	"No one is talking to you",
+	"Remember when you bet on Florida and lost",
+	"Oi! Shut yer mouth cunt",
+	"Don't you have a video game to be sucking at"
+]
+
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
@@ -22,11 +36,13 @@ module.exports = {
 		let msg;
 		let msgIndex;
 		if(interaction.user.username == 'cprock5') {
-
+			const meanMsgListLength = colinHelloMessages.length;
+			msgIndex = getRandomInt(0,meanMsgListLength);
+			msg = colinHelloMessages[msgIndex];
 		} else {
 			const friendlyMsgListLenght = friendlyHelloMessages.length;
 			msgIndex = getRandomInt(0,friendlyMsgListLenght);
-			msg = friendlyHelloMessages[msgIndex].replace(interaction.user.username);
+			msg = friendlyHelloMessages[msgIndex].replace('*', `${interaction.user}`);
 		}
 		await interaction.reply(msg);
 	},
